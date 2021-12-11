ad1b177e1c22ab23dcae3f348b4d61219aaaed5d
Shazidur-Rahman <shazidur112818@gmail.com>
2021-12-11 21:49:01 +0600

    Browse config and open url

--- src/main/java/com/Base/BaseClass.java
+++ src/main/java/com/Base/BaseClass.java
@@ -2,6 +2,9 @@ package com.Base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverInfo;
 import org.openqa.selenium.opera.OperaDriver;
 
 public class BaseClass {
@@ -9,16 +12,57 @@ public class BaseClass {
     public static WebDriver driver;
 
     public static void main(String[] args) {
-        //chrome_launce();
-        opera_launce();
-
+        chrome_launce();
+        open_URL();
+        //opera_launce();
+        //firefox_launce();
+        //edge_launce();
+        chrome_close();
+        //opera_close();
+        //firefox_close();
+        //edge_close();
     }
     public static  void chrome_launce(){
-        System.setProperty("webdriver.chrome.driver","./src/main/resources/Driver/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","G:\\TestAutomation\\src\\main\\resources\\Drivers\\chromedriver.exe");
         driver = new ChromeDriver();
     }
     public static  void opera_launce(){
-        System.setProperty("webdriver.opera.driver","./src/main/resources/Driver/operadriver.exe");
+        System.setProperty("webdriver.opera.driver","G:\\TestAutomation\\src\\main\\resources\\Drivers\\operadriver.exe");
         driver = new OperaDriver();
+    }
+    public static  void firefox_launce(){
+        System.setProperty("webdriver.gecko.driver","G:\\TestAutomation\\src\\main\\resources\\Drivers\\geckodriver.exe");
+        driver = new FirefoxDriver();
+    }
+    public static  void edge_launce(){
+        System.setProperty("webdriver.edge.driver","G:\\TestAutomation\\src\\main\\resources\\Drivers\\msedgedriver.exe");
+        driver = new EdgeDriver();
+    }
+    public static  void chrome_close(){
+        driver.close();
+    }
+    public static  void firefox_close(){
+        driver.close();
+    }
+    public static  void opera_close(){
+        driver.close();
+    }
+    public static  void edge_close(){
+        driver.close();
+    }
+    public static  void chrome_quit(){
+        driver.quit();
+    }
+    public static  void firefox_quit(){
+        driver.quit();
+    }
+    public static  void opera_quit(){
+        driver.quit();
+    }
+    public static  void edge_quit(){
+        driver.quit();
+    }
+    public static void open_URL(){
+        driver.get("http://www.google.com");
     }
 }
